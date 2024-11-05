d71aa73e02776cd9d2bb9449c8c44350009d0c67
Meghana-nm <meghananm27@gmail.com>
2024-11-05 11:45:06 +0530

    update patient feature added

--- src/main/java/com/example/pharmassist/controller/PatientController.java
+++ src/main/java/com/example/pharmassist/controller/PatientController.java
@@ -7,15 +7,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pharmassist.exceptionhandler.FieldErrorExceptionHandler;
 import com.example.pharmassist.requestdtos.PatientRequest;
 import com.example.pharmassist.responsedtos.PatientResponse;
 import com.example.pharmassist.service.PatientService;
 import com.example.pharmassist.util.AppResponseBuilder;
+import com.example.pharmassist.util.ErrorStructure;
 import com.example.pharmassist.util.ResponseStructure;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -35,12 +42,39 @@ public class PatientController {
 		PatientResponse response=patientService.addPatient(patientRequest, pharmacyId);
 		return appResponseBuilder.success(HttpStatus.CREATED,"Patient Created", response);
 	}
-	
+
+	@Operation(description = "The endpoint can be used to find all patients associated with a pharmacy through the pharmacy ID",
+			responses = {
+					@ApiResponse(responseCode = "302", description = "Patients Found"),
+					@ApiResponse(responseCode = "404", description = "Patients not found by Pharmacy ID",
+					content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class))
+					})
+	})
+
 	@GetMapping("/pharmacies/{pharmacyId}/patients")
 	public ResponseEntity<ResponseStructure<List<PatientResponse>>> findAllPatientsByPharmacyId(@PathVariable String pharmacyId)
 	{
 		List<PatientResponse> response=patientService.findAllPatientByPharmacyId(pharmacyId);
 		return appResponseBuilder.success(HttpStatus.FOUND,"Patients associated with the pharmacy found", response);
+	}
+
+	@Operation(description = "The endpoint can be used to update the patient based on the unique patient ID within a specific pharmacy",
+			responses = {
+					@ApiResponse(responseCode = "302", description = "Patient Found"),
+					@ApiResponse(responseCode = "400", description = "Bad Request",
+					content = {
+							@Content(schema = @Schema(implementation = FieldErrorExceptionHandler.class))
+					}),
+					@ApiResponse(responseCode = "404", description = "Patient not found by ID",
+					content = {
+							@Content(schema = @Schema(implementation = ErrorStructure.class))
+					})
+	})
+	@PutMapping("/patients/{patientId}")
+	public ResponseEntity<ResponseStructure<PatientResponse>> updatePatient(@RequestBody PatientRequest patientRequest,  @PathVariable String patientId) {
+		PatientResponse response = patientService.updatePatient(patientRequest, patientId);
+		return appResponseBuilder.success(HttpStatus.OK, "Patient Updated", response);
 	}
 
 }
